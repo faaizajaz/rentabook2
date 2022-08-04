968c02e46bc2c70320d0491a852e9c8f911b2e45
faaiz <faaizajaz@gmail.com>
2022-08-04 22:31:02 +0500

    Media url added

--- rentabook2/settings.py
+++ rentabook2/settings.py
@@ -148,6 +148,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
