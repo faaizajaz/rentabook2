078b7caf608653e37678360f83869502caca9692
faaiz <faaizajaz@gmail.com>
2022-08-08 20:49:48 +0500

    Fixed error when no fiction results are found

--- bookquery/views.py
+++ bookquery/views.py
@@ -27,40 +27,46 @@ def BookQueryView(request, **kwargs):
 
             ########## FICTION #############
             if query.search_type == "Fiction":
-                num_pages = query.get_num_pages_fiction()
-                results = query.search_fiction(num_pages)
-                # print(results)
+                try:
+                    num_pages = query.get_num_pages_fiction()
+                    results = query.search_fiction(num_pages)
+                    # print(results)
 
-                if len(results) == 0:
+                    if len(results) == 0:
+                        print("## RENTABOOK ##: No mobi or epub found on Libgen")
+                    else:
+                        if results[0] == "timeout":
+                            print("## RENTABOOK ##: Search timed out.")
+                        else:
+                            print("## RENTABOOK ##: Mobi/epub results found on Libgen")
+
+                    json_results = json.dumps(results)
+                    request.session["search_results"] = results
+
+                    if len(results) > 0:
+                        if results[0] == "timeout":
+                            print("## RENTABOOK ##: Redirecting to search timeout page")
+                            return render(
+                                request, "bookquery/timeout.html", {"nodata": "nodata"}
+                            )
+                        else:
+                            print("## RENTABOOK ##: Redirecting to results page")
+                            return render(
+                                request,
+                                "bookquery/results.html",
+                                {
+                                    "results": results,
+                                    "json_results": json_results,
+                                    "user": query.user.username,
+                                },
+                            )
+                    else:
+                        print("## RENTABOOK ##: Redirecting to no results page")
+                        return render(
+                            request, "bookquery/noresults.html", {"nodata": "nodata"}
+                        )
+                except:
                     print("## RENTABOOK ##: No mobi or epub found on Libgen")
-                else:
-                    if results[0] == "timeout":
-                        print("## RENTABOOK ##: Search timed out.")
-                    else:
-                        print("## RENTABOOK ##: Mobi/epub results found on Libgen")
-
-                json_results = json.dumps(results)
-                request.session["search_results"] = results
-
-                if len(results) > 0:
-                    if results[0] == "timeout":
-                        print("## RENTABOOK ##: Redirecting to search timeout page")
-                        return render(
-                            request, "bookquery/timeout.html", {"nodata": "nodata"}
-                        )
-                    else:
-                        print("## RENTABOOK ##: Redirecting to results page")
-                        return render(
-                            request,
-                            "bookquery/results.html",
-                            {
-                                "results": results,
-                                "json_results": json_results,
-                                "user": query.user.username,
-                            },
-                        )
-                else:
-                    print("## RENTABOOK ##: Redirecting to no results page")
                     return render(
                         request, "bookquery/noresults.html", {"nodata": "nodata"}
                     )
