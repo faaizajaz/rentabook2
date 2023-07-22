4a1444f79994973d04a42f06a65dddeb23c138cb
faaiz <faaizajaz@gmail.com>
2023-07-22 16:34:35 +0500

    Nothing

--- bookquery/apps.py
+++ bookquery/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class BookqueryConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bookquery'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bookquery"
