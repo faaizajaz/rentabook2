93c2e3bc30f4d25a41578e0a8ef5f25429bc15d3
faaiz <faaizajaz@gmail.com>
2023-07-22 16:11:22 +0500

    Added download count model to admin

--- bookquery/admin.py
+++ bookquery/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import BookQuery
+from .models import BookQuery, DownloadCount
 
 admin.site.register(BookQuery)
+admin.site.register(DownloadCount)
 
 # Register your models here.
